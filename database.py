b0a10359d09b9947f0e87c1a1493188f7c3d025f
David Ehnert <17857925+DavidLokison@users.noreply.github.com>
2025-12-26 00:10:10 +0100

    feat: add healthcheck and database connection pool
    closes: #1

---
+++ database.py
@@ -0,0 +1,20 @@
+import mysql.connector
+from mysql.connector import pooling
+import os
+
+try:
+    pool = mysql.connector.pooling.MySQLConnectionPool(
+        pool_name="herald",
+        pool_size=os.getenv('HERALD_POOL_SIZE', 5),
+        host=os.getenv('DB_HOST', 'localhost'),
+        port=os.getenv('DB_PORT', 3306),
+        database=os.getenv('DB_NAME', ''),
+        user=os.getenv('DB_USER', ''),
+        password=os.getenv('DB_PASSWORD', ''),
+    )
+except mysql.connector.Error as err:
+    print(f"Error creating pool: {err}")
+
+def connection():
+    """Fetch a connection from the pool."""
+    return pool.get_connection()
--- main.py
+++ main.py
@@ -1,3 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from pydantic import BaseModel
+import database
+
+from time import time
 
 app = FastAPI()
+
+
+def fetch_connection():
+    with database.connection() as connection:
+        with connection.cursor(dictionary=True) as cursor:
+            try:
+                yield cursor
+                connection.commit()
+            except Exception:
+                connection.rollback()
+                raise
+
+
+@app.get("/health")
+def check_health(db=Depends(fetch_connection)):
+    try:
+        tic = time()
+        db.execute("WITH t_health (base) AS (SELECT DATABASE() = 'hbz-registrations') SELECT IF(base, 'ok', 'warn') AS status, IF(base, '', 'wrong database selected') AS message FROM t_health")
+        toc = time()
+        result = db.fetchone()
+        result['ping'] = 1000 * (toc - tic)
+        return result
+    except Exception:
+        return { status: 'fatal', message: 'backend service unreachable' }
