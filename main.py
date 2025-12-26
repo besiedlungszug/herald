d4c31e4dd8954ec99b241815e75fffb0b43ab745
David Ehnert <17857925+DavidLokison@users.noreply.github.com>
2025-12-27 00:01:05 +0100

    fix: apply a default error handler to the mysql interface error
    Also makes general error handling more verbose as that was needed
    for introducing the new response codes.
    
    closes: #5

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
 import textwrap
-from fastapi import FastAPI, Query, Path, Depends, HTTPException
+from fastapi import FastAPI, Query, Path, Depends, Request, HTTPException
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field
 import database
 
 from time import time
+import mysql.connector
+
+
+class Message(BaseModel):
+    status: int = Field(..., example=503, description="Response Status Code")
+    message: str = Field(..., example="Service Unavailable", description="Response Status Message")
+
+
+class OptionalPingMessage(Message):
+    status: int = Field(..., example=500, description="Response Status Code")
+    message: str = Field(..., example="Internal Server Error", description="Response Status Message")
+    ping: float = Field(None, example=0.47, description="Response Query Time (if available)")
+
+
+class PingMessage(Message):
+    status: int = Field(..., example=200, description="Response Status Code")
+    message: str = Field(..., example="OK", description="Response Status Message")
+    ping: float = Field(..., example=0.47, description="Response Query Time")
+
 
 app = FastAPI()
+default_responses = {
+        503: { 'model': Message, 'description': "Backend is unreachable." },
+    }
 
 
 def fetch_connection():
@@ -19,25 +42,42 @@ def fetch_connection():
                 raise
 
 
-class HealthCheckResponse(BaseModel):
-    status: str = Field(..., example="ok", description=textwrap.dedent("""\
-    The status of the backend service
+@app.exception_handler(mysql.connector.errors.InterfaceError)
+def handle_database_exception(request: Request, e: mysql.connector.errors.InterfaceError):
+    return JSONResponse(
+            status_code = 503,
+            content = {
+                'status': 503,
+                'message': 'Service Unavailable: Backend Unreachable',
+            },
+        )
 
-    - `ok`: all systems nominal
-    - `warn`: backend database sent a warning
-    - `fatal`: backend service could not be reached"""))
-    message: str = Field(..., example='', description="An additional message providing details on a potential error")
-    ping: float = Field(None, example=0.47, description="The time used to query the backend database")
 
-
-@app.get("/health", response_model=HealthCheckResponse)
+@app.get(
+        "/health",
+        response_model = PingMessage,
+        response_description = "Health check was successful.",
+        responses = {
+            500: { 'model': OptionalPingMessage, 'description': "- The backend service failed its healthcheck.\n- The healthcheck query failed." },
+            **default_responses,
+        },
+    )
 def check_health(db=Depends(fetch_connection)):
     try:
         tic = time()
-        db.execute("WITH t_health (base) AS (SELECT DATABASE() = 'hbz-registrations') SELECT IF(base, 'ok', 'warn') AS status, IF(base, '', 'wrong database selected') AS message FROM t_health")
+        db.execute("WITH t_health (base) AS (SELECT DATABASE() = 'hbz-registrations') SELECT IF(base, 200, 500) AS status, IF(base, 'OK', 'Internal Server Error: Bad Database') AS message FROM t_health")
         toc = time()
         result = db.fetchone()
         result['ping'] = 1000 * (toc - tic)
-        return result
+        return JSONResponse(
+                status_code = result['status'],
+                content = result,
+            )
     except Exception:
-        return { status: 'fatal', message: 'backend service unreachable' }
+        return JSONResponse(
+                status_code = 500,
+                content = {
+                    'status': 500,
+                    'message': 'Internal Server Error: Unknown Error',
+                },
+            )
